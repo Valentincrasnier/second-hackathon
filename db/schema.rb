59372c6e116865694562bc72fcc62936638ae126
benjamin <benjamin.lebas@yahoo.fr>
2016-05-04 11:47:30 +0200

    modif responsive

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503145145) do
+ActiveRecord::Schema.define(version: 20160504061511) do
 
   create_table "challenges", force: :cascade do |t|
     t.string   "description"
