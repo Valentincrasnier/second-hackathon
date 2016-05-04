c30866f95a0994ce1b70eb3374c1f2651f914e13
benjamin <benjamin.lebas@yahoo.fr>
2016-05-04 13:55:26 +0200

    scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,114 +6,114 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Ben = User.create(
-#   pseudo: "Ben", 
-#   birthday: DateTime.new(1989,1,24),
-#   sex: true, 
-#   email: "ben@gmail.com", 
-#   password: "12345678")
+Ben = User.create(
+  pseudo: "Ben", 
+  birthday: DateTime.new(1989,1,24),
+  sex: "Masculin", 
+  email: "ben@gmail.com", 
+  password: "12345678")
 
-# Lanla = User.create(
-#   pseudo: "Lanla", 
-#   birthday: DateTime.new(1985,6,4),
-#   sex: false, 
-#   email: "lanla@gmail.com", 
-#   password: "12345678")
+Lanla = User.create(
+  pseudo: "Lanla", 
+  birthday: DateTime.new(1985,6,4),
+  sex: "Féminin", 
+  email: "lanla@gmail.com", 
+  password: "12345678")
 
-# Tultri = User.create(
-#   pseudo: "Tultri", 
-#   birthday: DateTime.new(1975,8,20),
-#   sex: true, 
-#   email: "tultri@gmail.com", 
-#   password: "12345678")
+Tultri = User.create(
+  pseudo: "Tultri", 
+  birthday: DateTime.new(1975,8,20),
+  sex: "Masculin", 
+  email: "tultri@gmail.com", 
+  password: "12345678")
 
-# Urgmog = User.create(
-#   pseudo: "Urgmog", 
-#   birthday: DateTime.new(1991,6,17),
-#   sex: true, 
-#   email: "urgmog@gmail.com", 
-#   password: "12345678")
+Urgmog = User.create(
+  pseudo: "Urgmog", 
+  birthday: DateTime.new(1991,6,17),
+  sex: "Masculin", 
+  email: "urgmog@gmail.com", 
+  password: "12345678")
 
-# Pwenlen = User.create(
-#   pseudo: "Pwenlen", 
-#   birthday: DateTime.new(1989,3,13),
-#   sex: false, 
-#   email: "pwenlen@gmail.com", 
-#   password: "12345678")
+Pwenlen = User.create(
+  pseudo: "Pwenlen", 
+  birthday: DateTime.new(1989,3,13),
+  sex: "Féminin", 
+  email: "pwenlen@gmail.com", 
+  password: "12345678")
 
-# Kynune = User.create(
-#   pseudo: "Kynune", 
-#   birthday: DateTime.new(1985,5,20),
-#   sex: true, 
-#   email: "kynune@gmail.com", 
-#   password: "12345678")
+Kynune = User.create(
+  pseudo: "Kynune", 
+  birthday: DateTime.new(1985,5,20),
+  sex: "Masculin", 
+  email: "kynune@gmail.com", 
+  password: "12345678")
 
-# Ruslew = User.create(
-#   pseudo: "Ruslew", 
-#   birthday: DateTime.new(1980,7,10),
-#   sex: true, 
-#   email: "ruslew@gmail.com", 
-#   password: "12345678")
+Ruslew = User.create(
+  pseudo: "Ruslew", 
+  birthday: DateTime.new(1980,7,10),
+  sex: "Masculin", 
+  email: "ruslew@gmail.com", 
+  password: "12345678")
 
-# Urgubal = User.create(
-#   pseudo: "Urgubal", 
-#   birthday: DateTime.new(1990,1,14),
-#   sex: false, 
-#   email: "urgubal@gmail.com", 
-#   password: "12345678")
+Urgubal = User.create(
+  pseudo: "Urgubal", 
+  birthday: DateTime.new(1990,1,14),
+  sex: "Féminin", 
+  email: "urgubal@gmail.com", 
+  password: "12345678")
 
-# Dourtor = User.create(
-#   pseudo: "Dourtor", 
-#   birthday: DateTime.new(1979,7,22),
-#   sex: true, 
-#   email: "dourtor@gmail.com", 
-#   password: "12345678")
+Dourtor = User.create(
+  pseudo: "Dourtor", 
+  birthday: DateTime.new(1979,7,22),
+  sex: "Masculin", 
+  email: "dourtor@gmail.com", 
+  password: "12345678")
 
-# Linlen = User.create(
-#   pseudo: "Linlen", 
-#   birthday: DateTime.new(1986,2,19),
-#   sex: false, 
-#   email: "linlen@gmail.com", 
-#   password: "12345678")
+Linlen = User.create(
+  pseudo: "Linlen", 
+  birthday: DateTime.new(1986,2,19),
+  sex: "Féminin", 
+  email: "linlen@gmail.com", 
+  password: "12345678")
 
-# Cildesca = User.create(
-#   pseudo: "Cildesca", 
-#   birthday: DateTime.new(1988,11,9),
-#   sex: false, 
-#   email: "cildesca@gmail.com", 
-#   password: "12345678")
+Cildesca = User.create(
+  pseudo: "Cildesca", 
+  birthday: DateTime.new(1988,11,9),
+  sex: "Féminin", 
+  email: "cildesca@gmail.com", 
+  password: "12345678")
 
-# Tia = User.create(
-#   pseudo: "Tia", 
-#   birthday: DateTime.new(1987,7,27),
-#   sex: false, 
-#   email: "tia@gmail.com", 
-#   password: "12345678")
+Tia = User.create(
+  pseudo: "Tia", 
+  birthday: DateTime.new(1987,7,27),
+  sex: "Féminin", 
+  email: "tia@gmail.com", 
+  password: "12345678")
 
-# Skyhart = User.create(
-#   pseudo: "Skyhart", 
-#   birthday: DateTime.new(1994,6,13),
-#   sex: true, 
-#   email: "skyhart@gmail.com", 
-#   password: "12345678")
+Skyhart = User.create(
+  pseudo: "Skyhart", 
+  birthday: DateTime.new(1994,6,13),
+  sex: "Masculin", 
+  email: "skyhart@gmail.com", 
+  password: "12345678")
 
-# Moreeya = User.create(
-#   pseudo: "Moreeya", 
-#   birthday: DateTime.new(1984,3,18),
-#   sex: false, 
-#   email: "moreeya@gmail.com", 
-#   password: "12345678")
+Moreeya = User.create(
+  pseudo: "Moreeya", 
+  birthday: DateTime.new(1984,3,18),
+  sex: "Féminin", 
+  email: "moreeya@gmail.com", 
+  password: "12345678")
 
-# Ebbven = User.create(
-#   pseudo: "Ebbven", 
-#   birthday: DateTime.new(1987,1,12),
-#   sex: false, 
-#   email: "ebbven@gmail.com", 
-#   password: "12345678")
+Ebbven = User.create(
+  pseudo: "Ebbven", 
+  birthday: DateTime.new(1987,1,12),
+  sex: "Féminin", 
+  email: "ebbven@gmail.com", 
+  password: "12345678")
 
-# Luznarb = User.create(
-#   pseudo: "Luznarb", 
-#   birthday: DateTime.new(1986,8,6),
-#   sex: true, 
-#   email: "luznard@gmail.com", 
-#   password: "12345678")
+Luznarb = User.create(
+  pseudo: "Luznarb", 
+  birthday: DateTime.new(1986,8,6),
+  sex: "Masculin", 
+  email: "luznard@gmail.com", 
+  password: "12345678")
